6e8b6b682381b7b890e42282d9b7125aedbcaa6b
Pritesh Kadiwala <pk.45@hotmail.com>
2016-12-05 03:51:00 -0500

    Able to access the mongodb server database

--- tasklist/routes/tasks.js
+++ tasklist/routes/tasks.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://pkadiwal:1234@ds119738.mlab.com:19738/tasklistpritesh', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-    res.send('TASK API');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
 });
 
 module.exports = router;
