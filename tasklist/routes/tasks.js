7e2caa9fa94ee16b43addbf91b3989fb98afb7e3
Pritesh Kadiwala <pk.45@hotmail.com>
2016-12-05 04:21:19 -0500

    Fitting in the angular part

--- tasklist/routes/tasks.js
+++ tasklist/routes/tasks.js
@@ -38,4 +38,50 @@ router.post('/task', function(req, res, next){
     }
 });
 
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var updTask = {};
+    
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
+    
+    if(task.title){
+        updTask.title = task.title;
+    }
+    
+    if(!updTask){
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+    }
+});
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
